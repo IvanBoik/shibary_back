8a0908fa3b7feb3ad66018bbe22c0fb3202184bd
IvanBoik <boiko12332@gmail.com>
2026-03-15 21:14:36 +0400

    init

---
+++ src/main/kotlin/org/boiko/shibary_back/ShibaryBackApplication.kt
@@ -0,0 +1,11 @@
+package org.boiko.shibary_back
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShibaryBackApplication
+
+fun main(args: Array<String>) {
+    runApplication<ShibaryBackApplication>(*args)
+}
---
+++ src/test/kotlin/org/boiko/shibary_back/ShibaryBackApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.boiko.shibary_back
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShibaryBackApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
